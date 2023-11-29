9b6a580257869d6e1dcd25b06738417b0c30064d
neverlosepain <neverlosepain@gmail.com>
2023-11-29 04:29:43 -0500

    feat: auto key presser

---
+++ Auto Key Presser/MainFrm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Auto_Key_Presser
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.KeyTxtBox = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(167, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 11000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(14, 142);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Hotkey: CRTL + O";
+            // 
+            // KeyTxtBox
+            // 
+            this.KeyTxtBox.Location = new System.Drawing.Point(9, 35);
+            this.KeyTxtBox.Multiline = true;
+            this.KeyTxtBox.Name = "KeyTxtBox";
+            this.KeyTxtBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.KeyTxtBox.Size = new System.Drawing.Size(263, 61);
+            this.KeyTxtBox.TabIndex = 11;
+            this.KeyTxtBox.Text = "uuid\r\n";
+            this.KeyTxtBox.TextChanged += new System.EventHandler(this.KeyTxtBox_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(9, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(106, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Edit Sample Text";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 166);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.KeyTxtBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainFrm";
+            this.Text = "Visual Studio Code";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox KeyTxtBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Auto Key Presser/MainFrm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Auto_Key_Presser
+{
+    public partial class MainFrm : Form
+    {
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private KeyHandler ghk;
+
+        private int Interval = 10000;
+
+        public MainFrm()
+        {
+            InitializeComponent();
+            ghk = new KeyHandler(Constants.CTRL, Keys.O, this);
+            ghk.Register();
+            KeyTxtBox.Enabled = false;
+        }
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = Convert.ToInt32(Interval);
+
+            timer1.Start();
+            button1.Enabled = false;
+            button2.Enabled = true;
+
+            Process[] notepads = Process.GetProcessesByName("notepad");
+
+            if (notepads.Length > 0)
+            {
+                // Notepad is already running, bring it to the foreground
+                IntPtr hWnd = notepads[0].MainWindowHandle;
+                SetForegroundWindow(hWnd);
+            }
+            else
+            {
+                // Notepad is not running, start a new instance
+                Process.Start("notepad.exe");
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Process[] notepads = Process.GetProcessesByName("notepad");
+
+            if (notepads.Length > 0)
+            {
+                // Notepad is already running, bring it to the foreground
+                IntPtr hWnd = notepads[0].MainWindowHandle;
+                SetForegroundWindow(hWnd);
+            }
+            SendKeys.Send(KeyTxtBox.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void HandleHotkey()
+        {
+            if (button1.Enabled)
+            {
+                button1_Click(this, null);
+            }
+            else
+            {
+                button2_Click(this, null);
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Constants.WM_HOTKEY_MSG_ID)
+                HandleHotkey();
+            base.WndProc(ref m);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            KeyTxtBox.Enabled = checkBox1.Checked;  
+        }
+
+        private void KeyTxtBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
